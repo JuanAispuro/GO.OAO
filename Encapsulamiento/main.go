3bdede29a17ec49a5e29f50407b618e85a15632f
JuanAispuro <Juan.aispuro193@tectijuana.edu.mx>
2022-11-08 11:43:30 -0800

    modulo

--- Encapsulamiento/main.go
+++ Encapsulamiento/main.go
@@ -1,21 +1,21 @@
 package main
 
+import (
+	"github.com/JuanAispuro/GO.OAO/tree/main/Encapsulamiento/course"
+)
+
 func main() {
-	//Tambien no es necesario poner el identificador aun lado simplemente siguiendo el orden de entrada.
-	Go := &course.Course{
-		"Go Programación Orientada a Objetos",
-		15.99,
-		false,
-		[]uint{12, 56, 89},
-		map[uint]string{
-			1: "Introducción",
-			2: "Estructuras",
-			3: "Maps",
-		},
-	}
-	//(*) Desreferenciacion
-	Go.PrintClasess()
+	Go := course.New("Go desde cero", 0, false)
+	Go.UsersID = []uint{12, 56, 89}
+	Go.Clasess = map[uint]string{
+		1: "Introducción",
+		2: "Estructuras",
+		3: "Maps",
+	},
+
+		//(*) Desreferenciacion
+		Go.PrintClasess()
 	//& obtener la memoria y direccion
-	// Go.ChangePrice(67.12)
+	Go.ChangePrice(67.12)
 	// fmt.Println(Go.Price)
 }
--- course/course.go
+++ course/course.go
@@ -1,9 +1,9 @@
-package main
+package course
 
 import "fmt"
 
 //Type nombre struct {}
-type Course struct {
+type course struct {
 	Name    string
 	Price   float64
 	IsFree  bool
@@ -11,9 +11,20 @@ type Course struct {
 	Clasess map[uint]string
 }
 
+func NewCourse(name string, price float64, isFree bool) *course {
+	if price == 0 {
+		price = 30
+	}
+	return &course{
+		Name:   name,
+		Price:  price,
+		IsFree: isFree,
+	}
+}
+
 //Mandamos una copia, pero pasamos un puntero para que mande el bueno.
 //Si queremos actualizar una estructura con el (*)
-func (c *Course) ChangePrice(price float64) {
+func (c *course) ChangePrice(price float64) {
 	c.Price = price
 }
 
@@ -21,7 +32,7 @@ func (c *Course) ChangePrice(price float64) {
 
 //tipo de dato que vamos a vincularlo.
 //Si no queremos actualizar nada es con el normal.
-func (c *Course) PrintClasess() {
+func (c *course) PrintClasess() {
 	text := "Las clases son: "
 	for _, class := range c.Clasess {
 		text += class + ", "
